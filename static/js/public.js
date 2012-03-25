757c3b3542d6ca05d75c9ccac6ab8bc340d46dad
Bulain Peng <bulain@gmail.com>
2012-03-25 18:48:17 +0800

    refacter code for change locale

--- static/js/public.js
+++ static/js/public.js
@@ -7,13 +7,20 @@ $(document).ready(function() {
 });
 
 function destroy(){
-	if (confirm('Are you sure?')) { 
-		var f = document.createElement('form'); 
-		f.style.display = 'none'; 
-		this.parentNode.appendChild(f); 
-		f.method = 'POST'; 
-		f.action = this.href;
-		f.submit(); 
-	};
-	return false;
+    if (confirm('Are you sure?')) { 
+    	$("<form style='display:none' method='POST'></form>")
+        	.appendTo($(this).parent())
+        	.attr('action', this.href)
+        	.submit();
+    };
+    return false;
+}
+
+function requestLocale(locale){
+    $("<form style='display:none' method='POST'/>")
+        .attr('action', window.location.href)
+        .appendTo($('#login'))
+        .append($("<input type='hidden' name='request_locale'/>").val(locale))
+        .submit();
+    return false;
 }
