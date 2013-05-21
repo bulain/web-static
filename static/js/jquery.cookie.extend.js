55e343f72c61f628e0ad525a51b973c48730b5e3
Bulain peng <bulain@gmail.com>
2013-05-21 11:51:00 +0800

    format and fix bug for cookie menu

--- static/js/jquery.cookie.extend.js
+++ static/js/jquery.cookie.extend.js
@@ -1,33 +1,32 @@
 jQuery.subcookie = function (ckname, key, value, options) {
-	
-	//get value from cookie
-	if (arguments.length > 3 && String(value) !== "[object Object]") {
-    	options = jQuery.extend({}, options);
+  
+  //get value from cookie
+  if (arguments.length > 3 && String(options) === "[object Object]") {
+    options = jQuery.extend({}, options);
+  }else{
+    options = {};
+  }
+  
+  var ckvalue = jQuery.cookie(ckname, options) || "";
+
+  // get subvalue from cookie
+  var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
+  var storevalue = (result = new RegExp('(?:^|\|)' + encodeURIComponent(key) + '=([^\|]*)').exec(ckvalue)) ? decode(result[1]) : null;
+  
+  //add value into cookie
+  if (arguments.length > 2 && String(value) !== "[object Object]") {
+    value = value || "";
+    
+    //add key pair
+    if(storevalue === null){
+      ckvalue += '|' + key + '=' + value;
+    //replace key pair
     }else{
-        options = value || {};
+      ckvalue = ckvalue.replace('|' + key + '=' + storevalue, '|' + key + '=' + value);
     }
-    var ckvalue = jQuery.cookie(ckname, options) || "";
-	
-    // get subvalue from cookie
-    var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
-    var storevalue = (result = new RegExp('(?:^|\|)' + encodeURIComponent(key) + '=([^\|]*)').exec(ckvalue)) ? decode(result[1]) : null;
-	
-	//add value into cookie
-	if (arguments.length > 2 && String(value) !== "[object Object]") {
-		options = jQuery.extend({}, options);
-		
-		value = value || "";
-		
-		//add key pair
-		if(storevalue === null){
-			ckvalue += '|' + key + '=' + value;
-		//replace key pair
-		}else{
-			ckvalue = ckvalue.replace('|' + key + '=' + storevalue, '|' + key + '=' + value);
-		}
-		
-		return jQuery.cookie(ckname, ckvalue, options);
-	}
-	
-	return ((storevalue && storevalue.length>0) ? storevalue : null);
+    
+    return jQuery.cookie(ckname, ckvalue, options);
+  }
+  
+  return ((storevalue && storevalue.length>0) ? storevalue : null);
 };
